a7b271eb46da3caf48eb631c29fa3ddfbcb3c1b5
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-13 22:02:54 -0500

    Tests for blackjack

---
+++ src/blackjack.js
@@ -0,0 +1,48 @@
+/**
+ * Should return the basic numeric value of a card
+ * @param  {String} cardName
+ * @return {Number}
+ */
+function cardValue(cardName) {
+
+}
+
+/**
+ * Returns the numeric sum of two numbers
+ * @param  {Number} a
+ * @param  {Number} b
+ * @return {Number}
+ */
+function sum(a, b) {
+
+}
+
+/**
+ * Returns if a dealer should split doubles
+ * @param  {String} cardOne
+ * @param  {String} cardTwo
+ * @return {Boolean}
+ */
+function shouldSplit(cardOne, cardTwo) {
+
+}
+
+/**
+ * Returns if a dealer should hit given two cards
+ * @param  {String} cardOne
+ * @param  {String} cardTwo
+ * @return {Boolean}
+ */
+function shouldHit(cardOne, cardTwo) {
+
+}
+
+/**
+ * Returns what the dealer should do
+ * @param  {String} cardOne
+ * @param  {String} cardTwo
+ * @return {String}
+ */
+function selectAction(cardOne, cardTwo) {
+
+}
--- src/index.js
+++
@@ -1,3 +0,0 @@
-function shouldHit(cardOne, cardTwo) {
-
-}
